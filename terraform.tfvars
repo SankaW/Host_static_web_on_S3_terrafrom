923ee0adfc7341f4445c3cd23659b4c5fd38d168
SankaW <sdweerathunga5@gmail.com>
2024-03-31 18:14:05 -0400

    Initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+aws_region  = "us-east-1" # Select an AWS region that is geographically closer to your website viwers.
+bucket_name = "snk-statict-web" # Replace "snk-statict-web" with your unique S3 bucket name.
---
+++ your_static_website.com/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Example JS that could interact with the page, not needed for the static message.
+    console.log('Website loaded and script.js is working!');
+});
+
